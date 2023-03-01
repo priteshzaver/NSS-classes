08a06bcf631527823ccb2835fa6b6022b4a295a5
Pritesh Zaver <priteshzaver@gmail.com>
2023-03-01 09:12:56 -0600

    initial commit

---
+++ Company.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+
+namespace Classes
+{
+    public class Company
+    {
+        // Some readonly properties (let's talk about gets, baby)
+        public string Name { get; }
+        public DateTime CreatedOn { get; }
+
+        // Create a public property for holding a list of current employees
+        public List<Employee> Employees { get; set; }
+        /*
+            Create a constructor method that accepts two arguments:
+                1. The name of the company
+                2. The date it was created
+            The constructor will set the value of the public properties
+        */
+        public Company(string name, DateTime createdOn)
+        {
+            Name = name;
+            CreatedOn = createdOn;
+            Employees = new List<Employee>();
+        }
+
+        public void ListEmployees()
+        {
+            foreach (Employee employee in Employees)
+            {
+                Console.WriteLine($"{employee.FullName} works for {Name} as {employee.Title} since {employee.StartDate}.");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an instance of a company. Name it whatever you like.
+            Company myCompany = new Company("Acme, Inc", DateTime.Now);
+            // Create three employees
+            Employee joey = new Employee
+            {
+                FirstName = "Joey",
+                LastName = "Tribbiani",
+                Title = "Chief Medical Officer",
+                StartDate = DateTime.Now
+            };
+            Employee chandler = new Employee
+            {
+                FirstName = "Chandler",
+                LastName = "Bing",
+                Title = "IT Procurements Manager",
+                StartDate = DateTime.Now
+            };
+            Employee rachael = new Employee
+            {
+                FirstName = "Rachael",
+                LastName = "Green",
+                Title = "Chief Fashion Office",
+                StartDate = DateTime.Now
+            };
+            // Assign the employees to the company
+            myCompany.Employees.Add(joey);
+            myCompany.Employees.Add(chandler);
+            myCompany.Employees.Add(rachael);
+            /*
+                Iterate the company's employee list and generate the
+                simple report shown above
+            */
+            myCompany.ListEmployees();
+        }
+    }
+}
